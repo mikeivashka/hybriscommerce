0c55a4273b958558ec977216f805a28e81a2460d
mikeivashka <mike.ivashka@gmail.com>
2021-06-27 15:07:56 +0300

    Task 3: introduced Questions addon & itemtype

--- bin/custom/training/traininginitialdata/src/org/training/initialdata/setup/InitialDataSystemSetup.java
+++ bin/custom/training/traininginitialdata/src/org/training/initialdata/setup/InitialDataSystemSetup.java
@@ -19,8 +19,13 @@ import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
+import de.hybris.platform.servicelayer.impex.ImportConfig;
+import de.hybris.platform.servicelayer.impex.ImportResult;
+import de.hybris.platform.servicelayer.impex.ImportService;
+import de.hybris.platform.servicelayer.impex.impl.StreamBasedImpExResource;
 import org.training.initialdata.constants.TrainingInitialDataConstants;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,99 +37,113 @@ import org.springframework.beans.factory.annotation.Required;
  * This class provides hooks into the system's initialization and update processes.
  */
 @SystemSetup(extension = TrainingInitialDataConstants.EXTENSIONNAME)
-public class InitialDataSystemSetup extends AbstractSystemSetup
-{
-	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger(InitialDataSystemSetup.class);
+public class InitialDataSystemSetup extends AbstractSystemSetup {
+    @SuppressWarnings("unused")
+    private static final Logger LOG = Logger.getLogger(InitialDataSystemSetup.class);
 
-	private static final String IMPORT_CORE_DATA = "importCoreData";
-	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
-	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+    private static final String IMPORT_CORE_DATA = "importCoreData";
+    private static final String IMPORT_SAMPLE_DATA = "importSampleData";
+    private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
 
-	private CoreDataImportService coreDataImportService;
-	private SampleDataImportService sampleDataImportService;
+    private CoreDataImportService coreDataImportService;
+    private SampleDataImportService sampleDataImportService;
+    private ImportService importService;
 
-	/**
-	 * Generates the Dropdown and Multi-select boxes for the project data import
-	 */
-	@Override
-	@SystemSetupParameterMethod
-	public List<SystemSetupParameter> getInitializationOptions()
-	{
-		final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
+    @Required
+    public void setImportService(ImportService importService) {
+        this.importService = importService;
+    }
 
-		params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
-		params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
-		params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
-		// Add more Parameters here as you require
+    /**
+     * Generates the Dropdown and Multi-select boxes for the project data import
+     */
+    @Override
+    @SystemSetupParameterMethod
+    public List<SystemSetupParameter> getInitializationOptions() {
+        final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
 
-		return params;
-	}
+        params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
+        params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
+        params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
+        // Add more Parameters here as you require
 
-	/**
-	 * Implement this method to create initial objects. This method will be called by system creator during
-	 * initialization and system update. Be sure that this method can be called repeatedly.
-	 * 
-	 * @param context
-	 *           the context provides the selected parameters and values
-	 */
-	@SystemSetup(type = Type.ESSENTIAL, process = Process.ALL)
-	public void createEssentialData(final SystemSetupContext context)
-	{
-		// Add Essential Data here as you require
-	}
+        return params;
+    }
 
-	/**
-	 * Implement this method to create data that is used in your project. This method will be called during the system
-	 * initialization. <br>
-	 * Add import data for each site you have configured
-	 *
-	 * <pre>
-	 * final List<ImportData> importData = new ArrayList<ImportData>();
-	 *
-	 * final ImportData sampleImportData = new ImportData();
-	 * sampleImportData.setProductCatalogName(SAMPLE_PRODUCT_CATALOG_NAME);
-	 * sampleImportData.setContentCatalogNames(Arrays.asList(SAMPLE_CONTENT_CATALOG_NAME));
-	 * sampleImportData.setStoreNames(Arrays.asList(SAMPLE_STORE_NAME));
-	 * importData.add(sampleImportData);
-	 *
-	 * getCoreDataImportService().execute(this, context, importData);
-	 * getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
-	 *
-	 * getSampleDataImportService().execute(this, context, importData);
-	 * getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
-	 * </pre>
-	 *
-	 * @param context
-	 *           the context provides the selected parameters and values
-	 */
-	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
-	public void createProjectData(final SystemSetupContext context)
-	{
-		/*
-		 * Add import data for each site you have configured
-		 */
-	}
+    /**
+     * Implement this method to create initial objects. This method will be called by system creator during
+     * initialization and system update. Be sure that this method can be called repeatedly.
+     *
+     * @param context the context provides the selected parameters and values
+     */
+    @SystemSetup(type = Type.ESSENTIAL, process = Process.ALL)
+    public void createEssentialData(final SystemSetupContext context) {
+    }
 
-	public CoreDataImportService getCoreDataImportService()
-	{
-		return coreDataImportService;
-	}
+    /**
+     * Implement this method to create data that is used in your project. This method will be called during the system
+     * initialization. <br>
+     * Add import data for each site you have configured
+     *
+     * <pre>
+     * final List<ImportData> importData = new ArrayList<ImportData>();
+     *
+     * final ImportData sampleImportData = new ImportData();
+     * sampleImportData.setProductCatalogName(SAMPLE_PRODUCT_CATALOG_NAME);
+     * sampleImportData.setContentCatalogNames(Arrays.asList(SAMPLE_CONTENT_CATALOG_NAME));
+     * sampleImportData.setStoreNames(Arrays.asList(SAMPLE_STORE_NAME));
+     * importData.add(sampleImportData);
+     *
+     * getCoreDataImportService().execute(this, context, importData);
+     * getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+     *
+     * getSampleDataImportService().execute(this, context, importData);
+     * getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
+     * </pre>
+     *
+     * @param context the context provides the selected parameters and values
+     */
+    @SystemSetup(type = Type.PROJECT, process = Process.ALL)
+    public void createProjectData(final SystemSetupContext context) {
+        LOG.info("Starting custom project data loading for the questions extension");
+        impexImport("/traininginitialdata/import/sampledata/questions/projectdata-questions.impex");
+        LOG.info("Custom project data loading for the questions extension completed.");
+    }
 
-	@Required
-	public void setCoreDataImportService(final CoreDataImportService coreDataImportService)
-	{
-		this.coreDataImportService = coreDataImportService;
-	}
+    public CoreDataImportService getCoreDataImportService() {
+        return coreDataImportService;
+    }
 
-	public SampleDataImportService getSampleDataImportService()
-	{
-		return sampleDataImportService;
-	}
+    @Required
+    public void setCoreDataImportService(final CoreDataImportService coreDataImportService) {
+        this.coreDataImportService = coreDataImportService;
+    }
 
-	@Required
-	public void setSampleDataImportService(final SampleDataImportService sampleDataImportService)
-	{
-		this.sampleDataImportService = sampleDataImportService;
-	}
+    public SampleDataImportService getSampleDataImportService() {
+        return sampleDataImportService;
+    }
+
+    @Required
+    public void setSampleDataImportService(final SampleDataImportService sampleDataImportService) {
+        this.sampleDataImportService = sampleDataImportService;
+    }
+
+    protected boolean impexImport(final String filename) {
+        final String message = TrainingInitialDataConstants.EXTENSIONNAME + " impexing [" + filename + "]...";
+        try (final InputStream resourceAsStream = getClass().getResourceAsStream(filename)) {
+            LOG.info(message);
+            final ImportConfig importConfig = new ImportConfig();
+            importConfig.setScript(new StreamBasedImpExResource(resourceAsStream, "UTF-8"));
+            importConfig.setLegacyMode(Boolean.FALSE);
+            final ImportResult importResult = importService.importData(importConfig);
+            if (importResult.isError()) {
+                LOG.error(message + " FAILED");
+                return false;
+            }
+        } catch (final Exception e) {
+            LOG.error(message + " FAILED", e);
+            return false;
+        }
+        return true;
+    }
 }
